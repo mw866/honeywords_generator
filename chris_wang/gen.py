c53d099cb1d0a98654991699cb8773b4c5aa7fc9
Chris M Wang <mw866@cornell.edu>
2016-10-31 17:13:45 -0400

    Functional for discussion

--- chris_wang/gen.py
+++ chris_wang/gen.py
@@ -24,72 +24,25 @@ import random
 import sys
 import string
 
-
-def read_infile(filename):
-    infile_list = [ ]
-    lines = open(filename,"r").readlines()
-    for line in lines:
-        infile_list.extend( line.split() )
-    return infile_list
-
- #[chris] To prove the original noise generator by using disctionaries link: https://wiki.skullsecurity.org/Passwords
-
-def make_password(real_list, train_list): # make a random password like those in given password list
-    for real_pw in real_list:
-        if real in real_list:
-            sweet = random.choice(train_list)
-        else:
-            sweet = make_password_like_ari(real_list)
-    return sweet_list
-
-def generate_sweet( n, real_list, train_list):
-    ans = [ ]
-    for t in range( n ):
-        pw = make_password(real_list, train_list)
-        ans.append( pw )
-    return ans
-
-def main():
-    n = int(sys.argv[1])     # get number of passwords desired
-    real_filename = sys.argv[1]    
-    sweet_filename = sys.argv[2] 
-    train_filename = sys.argv[3] 
-
-    real_list = read_infile(real_filename)
-    train_list = read_infile(train_filename)
-    sweet_list = generate_sweet(n, real_list, train_list)     # generate passwords
-    random.shuffle(sweet_list)     # shuffle their order
-
-    printing_wanted = True     # print if desired
-    if printing_wanted:
-        for sweet in sweet_list:
-            print (sweet)
-
-main()
-
-def make_password_like_ari(pw_list):
-    """ 
-    make a random password like those in given password list
-    """
+def make_password_like_ari(real, train_list):
+    #make a random password like those in given password list
     if random.random() < tn:
         return tough_nut()
     # start by choosing a random password from the list
     # save its length as k; we'll generate a new password of length k
-    k = len(random.choice(pw_list))
+    #k = len(real)
     # create list of all passwords of length k; we'll only use those in model
-    L = [ pw for pw in pw_list if len(pw) == k ]
+    L = [ pw for pw in train_list if len(pw) == len(real) ]
     nL = len(L)
     # start answer with the first char of that random password
-    # row = index of random password being used 
-    row = random.randrange(nL)
+ 
+    row = random.randrange(nL)    # index of random password being used 
     ans = L[row][:1]                  # copy first char of L[row] 
     j = 1                             # j = len(ans) invariant
-    while j < k:                      # build up ans char by char
+    while j < len(real):              # build up ans char by char
         p = random.random()           # randomly decide what to do next, based on p
-        # here p1 = prob of action 1
-        #      p2 = prob of action 2
-        #      p3 = prob of action 3
-        #      p1 + p2 + p3 = 1.00
+        #  p1 = prob of action 1, p2 = prob of action 2, p3 = prob of action 3
+        #  p1 + p2 + p3 = 1.00
         if p<p1:
             action = "action_1"
         elif p<p1+p2:
@@ -112,7 +65,7 @@ def make_password_like_ari(pw_list):
             ans = ans + L[row][j]
             j = j + 1
     if (nL > 0 or nD > 0 or nS > 0) and not syntax(ans): 
-        return make_password(pw_list)
+        return make_password_like_ari(real, train_list)
     return ans
 
 def tough_nut():
@@ -146,3 +99,51 @@ def syntax(p):
     if L >= nL and D >= nD and S >= nS:
         return True
     return False
+#===End From the origianl gen.py by Ari and Ron===
+
+def read_infile(filename):
+    infile_list = [ ]
+    lines = open(filename,"r").readlines()
+    for line in lines:
+        infile_list.extend( line.split() )
+    return infile_list
+
+ #[chris] To prove the original noise generator by using disctionaries link: https://wiki.skullsecurity.org/Passwords
+
+def make_sweet(real, train_list): # make a random password like those in given password list
+    if real in train_list:
+        sweet = random.choice(train_list)
+    else:
+        sweet = make_password_like_ari(real, train_list)
+    return sweet
+
+def generate_sweet_dict( n, real_list, train_list):
+    sweet_dict = {}
+    for real in real_list:
+        sweet_list = []
+        for t in range( n ):
+            sweet = make_sweet(real, train_list)
+            sweet_list.append( sweet )
+        sweet_dict[real]=sweet_list
+
+    return sweet_dict
+
+def main():
+    if len(sys.argv) < 5 :
+        print('Arguments: [number of sweets per real] [input-real filename] [output-sweet filename] [train_filename]')
+    n = int(sys.argv[1])     # get number of passwords desired
+    real_filename = sys.argv[2]    
+    sweet_filename = sys.argv[3] 
+    train_filename = sys.argv[4] 
+
+    real_list = read_infile(real_filename)
+    train_list = read_infile(train_filename)
+    sweet_dict = generate_sweet_dict(n, real_list, train_list)     # generate passwords
+    #random.shuffle(sweet_list)     # shuffle their order
+
+    print("Real Password\t\tSweet Passwords")
+    for real, sweet_list in sweet_dict.items():
+        print real, '\t\t', sweet_list
+    #<TODO> Export to outputfile
+main()
+
