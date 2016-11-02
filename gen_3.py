4a01a2137f558069ac7d2ca4d7fb122ad4e5b74a
Chris M Wang <mw866@cornell.edu>
2016-11-01 23:15:11 -0400

    add comments in code about the location of the full.txt on Github repo

--- gen_3.py
+++ gen_3.py
@@ -8,7 +8,7 @@ def main():
 	n = int(sys.argv[1])     # get number of passwords desired
 	real_filename = sys.argv[2]    
 	sweet_filename = sys.argv[3] 
-	train_filename = 'full.txt'  
+	train_filename = 'full.txt'  #The full.txt can be found at our Github Repo: https://github.com/mw866/cs5435_hw3
 	generate_from_train.generate(n ,real_filename, sweet_filename, train_filename)
 
 main()	
